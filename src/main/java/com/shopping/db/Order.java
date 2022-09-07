3c6f455cf973c954375ee50c06c165645053e99b
Evellyn Costa <ecosta.inf@gmail.com>
2022-09-07 15:33:27 -0300

    feature(app):start order client

---
+++ src/main/java/com/shopping/db/Order.java
@@ -0,0 +1,54 @@
+package com.shopping.db;
+
+import java.util.Date;
+
+public class Order {
+    private int userId;
+    private int orderId;
+    private int noOfItems;
+    private Double totalAmount;
+    private Date orderDate;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getNoOfItems() {
+        return noOfItems;
+    }
+
+    public void setNoOfItems(int noOfItems) {
+        this.noOfItems = noOfItems;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+}
+
+
+
